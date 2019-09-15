a042d80a8a50ad4b0a6a675dcbcd21e4e0cdc75b
nqhung07 <nqhung07@gmail.com>
2019-09-15 22:22:46 +0700

    push update 15/9/2019

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,35 +1,46 @@
+/* eslint-disable no-self-compare */
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
     Text,
     View,
     StyleSheet,
     StatusBar,
-} from 'react-native'
+    TextInput,
+    Button,
+} from 'react-native';
 
 import KeepAwake from 'react-native-keep-awake';
 import moment from 'moment';
-
-
 
 export default class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: moment().format("LTS"),
+            time: moment().format('LT'),
             day: moment().format('dddd'),
-            date: moment().format("ll"),
-        }
+            date: moment().format('ll'),
+            isAlarm: '',
+            timeAlarm: moment('9:32 AM', 'hh:mm a').format('LT'),
+            timeSet: '',
+        };
     }
 
-    setTime(){
+    setTime() {
         setTimeout(() => {
             this.setState({
-                time: moment().format("LTS"),
+                time: moment().format('LT'),
                 day: moment().format('dddd'),
-                date: moment().format("ll")
+                date: moment().format('ll'),
+                isAlarm: this.state.time === this.state.timeAlarm ? 'true' : 'false',
             });
         }, 1000);
+    }
+
+    setTimeAlarm(){
+        this.setState({
+            timeAlarm: moment(this.state.timeSet, 'hh:mm a').format('LT') 
+        });
     }
 
     componentDidMount() {
@@ -40,20 +51,35 @@ export default class Clock extends Component {
         this.setTime();
         return (
             <View style={styles.container}>
-				<StatusBar 
-                backgroundColor = "#000"
-                barStyle = "light-content"
+                <StatusBar
+                    backgroundColor="#000"
+                    barStyle="light-content"
                 />
-				<Text style={styles.timeText}>
-					{this.state.time}
-				</Text>
-				<Text style={styles.dateText}>
-                    {this.state.day}, {this.state.date}
-				</Text>
+                <Text style={styles.timeText}>
+                    {this.state.timeAlarm}
+                </Text>
+                <Text style={styles.timeText}>
+                    {this.state.time}
+                </Text>
+                <Text style={styles.dateText}>
+                    {this.state.isAlarm}, {this.state.day}, {this.state.date}
+                </Text>
 
-				<KeepAwake />
-			</View>
-        )
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="--:-- --"
+                    value={this.state.timeSet}
+                    onChangeText={(text) => this.setState({ timeSet: text })}
+                />
+
+                <Button
+                    title = "SAVE"
+                    onPress = {()=>this.setTimeAlarm()}
+                />
+
+                <KeepAwake />
+            </View>
+        );
     }
 }
 
@@ -61,7 +87,7 @@ export default class Clock extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000',
+        // backgroundColor: '#000',
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -72,5 +98,13 @@ const styles = StyleSheet.create({
     dateText: {
         color: '#999999',
         fontSize: 30,
-    }
+    },
+    timeInput: {
+        backgroundColor: 'skyblue',
+        width: '50%',
+        borderColor: 'red',
+        borderWidth: 2,
+        color: 'red',
+    },
 })
+    ;
